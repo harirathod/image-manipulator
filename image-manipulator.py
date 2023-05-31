c2927c0d9069ddfc291c79781d3c358047365eb8
harirathod <h4ri.rathod@gmail.com>
2023-05-31 14:17:07 +0100

    Implement display_image and save_image functions

--- image-manipulator.py
+++ image-manipulator.py
@@ -15,9 +15,10 @@ def convert_image_to_grayscale(image_path: str) -> np.ndarray:
     @throws FileNotFoundException If the image could not be read.
     @return The grayscale image, represented as a two-dimensional array (np.ndarray).
     """
-    image = cv.imread(image_path)
+    image = cv.imread(image_path, cv.IMREAD_UNCHANGED)
+
     if image is None:
-         raise FileNotFoundError(f'Image \'{image_path}\' could not be read.')
+         raise FileNotFoundError(f'Image \'{image_path}\' could not be found.')
     if image.ndim < 3:
         raise Exception("Image should already be grayscale.")
     # An image with ndim of 3 has width x, height y, and each pixel in the third-dimension is composed of 3 rgb colours.
@@ -26,6 +27,18 @@ def convert_image_to_grayscale(image_path: str) -> np.ndarray:
     temp = np.mean(image, axis=2).astype('uint8')
     return temp
 
-def display_image(image: np.ndarray) -> None:
-    cv.imshow("My Image", image)
+def display_image(image: np.ndarray, title_of_window: str = "My Image") -> str:
+    """
+    Displays the image in a window.
+    
+    The window closes once the user presses any key."""
+    cv.imshow(title_of_window, image)
+    # Display the image until the user presses any key, upon which the image should close.
+    cv.waitKey(0)
 
+def save_image(image: np.ndarray, name_of_image: str = "my-edited-image.jpg") -> None:
+    cv.imwrite(filename=name_of_image, img=image)
+
+image = convert_image_to_grayscale(sys.argv[1])
+display_image(image)
+save_image(image)
