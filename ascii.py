77c0c0e49161cceb1f6cce41dcf27618d3c185ce
harirathod <h4ri.rathod@gmail.com>
2023-06-03 22:02:26 +0100

    Calculate darkness ratios with PIL

--- ascii.py
+++ ascii.py
@@ -9,9 +9,44 @@ def colour_to_ascii(colour: int) -> str:
     match colour:
         case _ if colour < 10:
             return 
+
+# The purpose of using PIL, is because I want to calculate the darkness of each ASCII character,
+
+def get_darkness(text: str) -> float:
+    """
+    Get the darkness value of a character as a decimal between 0 and 1. 0 represents no darkness.
+    For example, ' ' (the space character) has no darkness.
+    """
+
+    font = ImageFont.truetype('Courier New.ttf', 30)
+
+    # Get the box dimensions of the text, from which, we can calculate the size of the text.
+    size = font.getsize(text)
+
+    # Create a grayscale image.
+    image = Image.new('1', (size), 2)
     
+    draw = ImageDraw.Draw(image)
+    draw.text((0, 0), text=text, font=font) # Render the text to the bitmap
+    image.save('text.png')
 
-#if __name__ == "__main__":
-    
+    for rownum in range(image.size[1]):
+        line = []
+        for colnum in range(image.size[0]):
+            if image.getpixel((colnum, rownum)): line.append(' .'),
+            else: line.append(' #'),
+        print (''.join(line))
 
 
+    # Count the number of pixels taken up by the text in its 'bounding box'.
+    pixel_count = [0]
+    for column in range(image.size[0]):
+        for row in range(image.size[1]):
+            if image.getpixel((column, row)) == 0:
+                pixel_count[0] += 1
+            
+    return pixel_count[0] / (image.size[0] * image.size[1])
+
+
+if __name__ == "__main__":
+    print(get_darkness(sys.argv[1]))
